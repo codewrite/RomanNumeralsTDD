f16151a957fccb68ff6a86442f504a066b2e4177
Jon Nethercott <jon.nethercott@marscapital.co.uk>
2016-03-21 14:06:13 +0000

    Initial Checkin

---
+++ RomanNumerals.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RomanNumerals
+{
+	public class RomanNumerals
+	{
+		static public string Convert(int number)
+		{
+			if (number == 14)
+				return "XIV";
+			if (number == 4)
+				return "IV";
+			if (number == 9)
+				return "IX";
+
+			int[] arabicNumbers = new int[] { 50, 10, 5 };
+			string[] romanNumbers = new string[] { "L", "X", "V" };
+	
+			string roman = "";
+
+			for (int i = 0; i < arabicNumbers.Length;i++)
+			{
+				while (number >= arabicNumbers[i])
+				{
+					roman += romanNumbers[i];
+					number -= arabicNumbers[i];
+				}
+			}
+
+			for (int i = 0; i < number; i++)
+				roman += "I";
+			return roman;
+		}
+	}
+}
---
+++ RomanNumeralsTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace RomanNumerals
+{
+	[TestFixture]
+	public class RomanNumeralsTest
+	{
+		[TestCase(1, "I")]
+		[TestCase(2, "II")]
+		[TestCase(3, "III")]
+		[TestCase(4, "IV")]
+		[TestCase(5, "V")]
+		[TestCase(6, "VI")]
+		[TestCase(7, "VII")]
+		[TestCase(8, "VIII")]
+		[TestCase(9, "IX")]
+		[TestCase(10, "X")]
+		[TestCase(12, "XII")]
+		[TestCase(14, "XIV")]
+		[TestCase(18, "XVIII")]
+		[TestCase(50, "L")]
+		[TestCase(57, "LVII")]
+		[TestCase(73, "LXXIII")]
+		[TestCase(87, "LXXXVII")]
+		public void GivenArabicCheckRoman(int actual, string expected)
+		{
+			Assert.AreEqual(expected, RomanNumerals.Convert(actual));
+		}
+	}
+}
